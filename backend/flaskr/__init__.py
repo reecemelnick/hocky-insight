d62390bab0ce24e49464548d3884c3d6d51e78ce
Reece Melnick <reecemelnick@MacBookPro.localdomain>
2026-01-22 16:06:14 -0800

    print

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -9,6 +9,7 @@ from .services.updateManager import UpdateManager
 
 # flask --app flaskr run --debug --port 8000
 def create_app(test_config=None):
+    print("App created...")
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     CORS(app)
